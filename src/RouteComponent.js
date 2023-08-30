89e89b7fa6f2c48e863f10fa1454b8af014c3054
minagrullon <minagrullon@pursuit.org>
2023-08-29 22:08:53 -0400

    setting up route component

---
+++ src/RouteComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+//Components
+import Login from "./components/login/Login";
+import Signup from "components/signup/Signup";
+
+export default function RouteComponent() {
+  return <div>RouteComponent</div>;
+}
