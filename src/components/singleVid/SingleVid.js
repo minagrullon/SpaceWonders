ae1064e7bfcefb700500831f30a83ccb8e8eaadf
minagrullon <minagrullon@pursuit.org>
2023-11-03 13:39:05 -0400

    working on the video results page, added some css

--- src/components/singleVid/SingleVid.js
+++ src/components/singleVid/SingleVid.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import "./SingleVid.scss";
 
 export default function SingleVid({ video, json }) {
   const [videoMp4, setVideoMp4] = useState("");
@@ -10,22 +11,24 @@ export default function SingleVid({ video, json }) {
     axios
       .get(json)
       .then((res) => {
-        console.log(res, "res");
-
         setVideoMp4(res.data.find((href) => href.includes("orig.mp4")));
       })
       .catch((err) => console.log(err));
   }, [json]);
 
   return (
-    <div>
-      {console.log(videoMp4)}
-      <h4>{video.data[0].title}</h4>
-      <img
-        style={{ height: "400px", width: "400px" }}
+    <div className="singleVideo">
+      {/* {console.log(videoMp4)} */}
+      <h4 className="singleVideo__title">{video.data[0].title}</h4>
+      <iframe
+        className="singleVideo__video"
+        // style={{ height: "400px", width: "400px" }}
         src={video.links[0].href}
+        allow="autoplay; fullscreen; picture-in-picture"
+        loading="lazy"
         alt={video.data[0].title}
-      />
+        title={video.data[0].title}
+      ></iframe>
     </div>
   );
 }
--- src/pages/VideoResults.js
+++ src/pages/VideoResults.js
@@ -15,31 +15,26 @@ export default function VideoResults() {
     axios
       .get(`https://images-api.nasa.gov/search?media_type=video`)
       // .get(`${URL}${search}`)
-      .then((res) =>
-        setVideos(
-          res.data.collection.items
-          // res.data.collection.items.filter(
-          //   (obj) => obj.data[0].media_type === "video"
-          // )
-        )
-      )
+      .then((res) => setVideos(res.data.collection.items))
       .catch((err) => console.warn(err));
   }, []);
   return (
     <div>
-      {/* add a search bar for the user to type their searches */}
-      Videos from NASA's collection
-      {console.log(videos)}
-      {videos.length > 0
-        ? videos.map((video, indx) => (
-            <div key={indx}>
-              <Link to={`/videos/${indx}`}>
-                <SingleVid video={video} json={video.href} />
-                {console.log(video.href)}
-              </Link>
-            </div>
-          ))
-        : null}
+      <h2 className="videoPageTitle">Videos from NASA's collection</h2>
+      <div className="videoResults">
+        {/* add a search bar for the user to type their searches */}
+        {console.log(videos)}
+        {videos.length > 0
+          ? videos.map((video, indx) => (
+              <div key={indx} className="videoResults__div">
+                <Link to={`/videos/${indx}`}>
+                  <SingleVid video={video} json={video.href} />
+                  {console.log(video.href)}
+                </Link>
+              </div>
+            ))
+          : null}
+      </div>
     </div>
   );
 }
