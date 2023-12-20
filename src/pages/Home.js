ed10cff864e089f8ae597c91f7c9615913f09cbc
minagrullon <minagrullon@pursuit.org>
2023-12-19 22:21:21 -0500

    changed the home page to feature news articles. currently just added a new api and tested if data shows

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,33 @@
 import React from "react";
-import Search from "../components/searchBar/Search";
-import ImageOfDay from "../components/imageOfTheDay/ImageOfDay";
-import FeaturedVideos from "../components/featuredVideos/FeaturedVideos";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const URL = process.env.REACT_APP_NEWS;
 
 export default function Home() {
   const hideDescription = document.body.querySelector(
     ".imageOfDayContainer__description"
   );
   //hide view of description when on the home page
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((res) => {
+        setArticles(res.data.articles);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="homePage">
       <h2 style={{ color: "white" }}>Explore with Space Wonders</h2>
-      <div className="homePage__searchBar">
-        <Search />
-      </div>
-      <div className="homePage__featured">
-        <FeaturedVideos />
+      {console.log(articles)}
+      <div className="homePage__articles">
+        {articles.map((el) => {
+          return <li>{el.title}</li>;
+        })}
       </div>
     </div>
   );
