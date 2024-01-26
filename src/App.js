f97cc180f31c5a0401df6ac19c5c21e75da0c3d9
minagrullon <minagrullon@pursuit.org>
2024-01-26 13:54:44 -0500

    added background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import RouteComponent from "./RouteComponent";
 import NavBar from "./components/navbar/Navbar";
 import "./App.scss";
+import backgroundVid from "./Assets/Background Stars Space.mp4";
 
 function App() {
   return (
     <div className="App">
+      <video
+        autoPlay
+        loop
+        muted
+        id="video"
+        className="background"
+        src={backgroundVid}
+      ></video>
       <NavBar />
       <RouteComponent />
     </div>
